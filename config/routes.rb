97a6359f9016dcb5f806e9fc80603c7cbf179d6f
D-R-E-D <shut.oleg4@gmail.com>
2020-01-10 16:20:35 +0200

    Fix mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'about/index'
-  get 'pages_controller/index'
-  root 'pages_controller#index'
+  get 'index' => 'pages#index', as: :index
+  get 'about' => 'pages#about', as: :about
+  root 'pages#index'
+   # get 'pages/about#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class AboutControllerTest < ActionDispatch::IntegrationTest
+class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get about_index_url
+    get pages_index_url
     assert_response :success
   end
 
